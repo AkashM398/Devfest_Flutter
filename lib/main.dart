540ce808249dfdd499da502ebb5f8a59f4ba33d0
AkashM398 <akashm1999@gmail.com>
2019-09-15 21:08:39 +0530

    code updated

--- lib/main.dart
+++ lib/main.dart
@@ -26,65 +26,34 @@ class HomePage extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Color(0xFF121214),
-        child: Stack(
-          children: <Widget>[
-            CustomMadeClipper(),
-            AppBar(title: Text('DevFest Chennai', 
-            style: TextStyle(color: Colors.white,
-            fontWeight: FontWeight.w400, 
-            fontFamily: 'Product_Sans',
-            fontSize: 25,)
+      backgroundColor: Color(0xFF121214),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            expandedHeight: 350.0,
+            floating: false,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset("assets/images/banner_dark.png"),
             ),
-              backgroundColor: Colors.transparent,
-              centerTitle: true,
+            centerTitle: true,
+            title: Text(
+              'Devfest Chennai',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Product_Sans',
+                fontSize: 22,
+              ),
+              ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(50),
+              ),
             ),
-
-          ],
-        ),
-      ),
+          ),
+        ],),
     );
   }
 }
 
-
-class CustomMadeClipper extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return ClipPath(
-      child: Container(
-        height: 450,
-        width: 400,
-        color: Color(0xFF202125),
-      ),
-      clipper: CustomBlueClipper(),
-    );
-  }
-}
-
-class CustomBlueClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-
-    path.lineTo(0.0, size.height - 50);
-
-    var firstControlPoint = Offset(size.width / 2, size.height);
-    var firstEndPoint = Offset(size.width, size.height - 50.0);
-    path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
-        firstEndPoint.dx, firstEndPoint.dy);
-
-
-    path.lineTo(size.width, size.height - 30);
-    path.lineTo(size.width, 0.0);
-    path.close();
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return true;
-  }
-}
